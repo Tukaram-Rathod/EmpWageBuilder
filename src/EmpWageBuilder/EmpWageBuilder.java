c352937f9fe095147e431baaf303ca1cba978202
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-23 20:41:28 +0530

    ADD. added empWageAttaindance program

--- src/EmpWageBuilder/EmpWageBuilder.java
+++ src/EmpWageBuilder/EmpWageBuilder.java
@@ -3,7 +3,14 @@ package EmpWageBuilder;
 public class EmpWageBuilder {
 public static void main(String[] args){
 		
-		System.out.println("Welcome To Employee Wage Computation Program");
+		//stem.out.println("Welcome To Employee Wage Computation Program");
+	//to check is emp present or not 
+		int Is_EmpPresent=1;
+		double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
+		if(empCheck == Is_EmpPresent)
+			System.out.println("EMPLOYEE IS PRESENT ");
+		else
+			System.out.println("EMPLOYEE IS ABSENT ");
 	
 	}
 }
