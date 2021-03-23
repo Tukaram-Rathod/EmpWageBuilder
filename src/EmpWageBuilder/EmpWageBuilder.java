afb0f372a51fdd7537c784ddac94128802021803
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-23 21:07:32 +0530

    ADD.added UC3

--- src/EmpWageBuilder/EmpWageBuilder.java
+++ src/EmpWageBuilder/EmpWageBuilder.java
@@ -1,22 +1,30 @@
 package EmpWageBuilder;
 
 public class EmpWageBuilder {
-public static void main(String[] args){
-		//calculate daily wages 
-	//constants
-	int IsEmpPresent=1;
-	int EMP_RATE_PER_HOUR =20;
-	//Variable
-	int empHrs =0;
-	int empWage =0;
-	//computation
-	double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
-	if(empCheck == IsEmpPresent)
-		empHrs =8;
-	else
-		empHrs =0;
-	empWage = empHrs *  EMP_RATE_PER_HOUR;
-	System.out.println("Emp WAGE " +empWage );
-
+	public static void main(String[] args){
+		//Constatnts
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int EMP_RATE_PER_HOUR =20;
+		//Variables
+		int empHrs =0;
+		int empWage =0;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10 ) % 3 ;
+		if(empCheck == IS_FULL_TIME) {
+		empHrs = 8;
+		empWage = empHrs *  EMP_RATE_PER_HOUR;
+		System.out.println("Emp WAGE " +empWage );
+		}
+		else if(empCheck == IS_PART_TIME) {
+			empHrs = 4;
+			empWage = empHrs *  EMP_RATE_PER_HOUR;
+			System.out.println("Emp WAGE " +empWage );
+		}
+		else { 
+			empHrs= 0;
+			empWage = empHrs *  EMP_RATE_PER_HOUR;
+			System.out.println("Emp WAGE " +empWage );
+		}
 	}
 }
